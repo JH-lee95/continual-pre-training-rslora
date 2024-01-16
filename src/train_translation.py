6fcaa4f8ad19d89929ddfd82aa6130c4ccf3f917
mingi.sung <mingi.sung@pwc.com>
2024-01-16 07:52:35 +0000

    term-mt initial commit

---
+++ src/train_translation.py
@@ -0,0 +1,197 @@
+from transformers import (
+  AutoModelForCausalLM, 
+  AutoTokenizer, 
+  BitsAndBytesConfig,
+  TrainingArguments,
+    Trainer,
+    get_cosine_with_hard_restarts_schedule_with_warmup,
+    AdamW,
+  )
+import bitsandbytes as bnb
+from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training, get_peft_model
+import os, wandb, platform, warnings,sys
+from datasets import load_dataset,Dataset,concatenate_datasets
+from trl import SFTTrainer,DataCollatorForCompletionOnlyLM
+import argparse
+import torch
+import numpy as np
+import random
+from align_chat_template import *
+from utils import *
+
+import ipdb
+from prettytable import PrettyTable
+import mlflow
+import chat_template
+from train_modules import *
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+
+    ## directories
+    parser.add_argument("--base_dir",type=str,default="/root/azurestorage/models",help="base directory to save logs and checkpoints")
+    parser.add_argument("--base_model_dir",type=str,required=True,help="local or huggingface hub directory of base model")
+    parser.add_argument("--ckpt_dir",type=str,default=None)
+    parser.add_argument("--mlflow_dir",type=str, default="mlruns")
+    parser.add_argument("--dataset_dir",type=str)
+
+    ## hyper parameters
+    parser.add_argument("--seed",type=int,default=42)
+    parser.add_argument("--learning_rate",type=float, default=1e-4)
+    parser.add_argument("--dropout_rate", type=float, default=0.1)
+    parser.add_argument("--max_len", type=int, default=4096)
+    parser.add_argument("--weight_decay", type=float, default=0.01)
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--epoch_size', type=int, default=1)
+    parser.add_argument("--gradient_accumulation_steps",type=int,default=1)
+    parser.add_argument("--gradient_checkpointing",type=bool,default=False,help="reducing required memory size but slower training")
+    parser.add_argument("--warmup_ratio",type=float,default=0.01)
+    parser.add_argument("--full_ft",action="store_true",help="full finetuning otherwise lora")
+    parser.add_argument("--num_save_per_epoch",type=int,default=3,help="number of saving(evaluating) per a epoch")
+    
+    ## etc
+    parser.add_argument("--expr_name",type=str,default=None, help="experiment name",required=True)
+    parser.add_argument("--expr_desc",type=str,help = "description for experiment", default = None)
+    parser.add_argument("--train",type=bool, default=True)
+    parser.add_argument("--test",type=bool, default=False)
+
+    return parser.parse_args()
+
+def set_environ(args):
+    os.environ["MLFLOW_EXPERIMENT_NAME"]=args.expr_name
+    # os.environ["MLFLOW_TRACKING_URI"]=args.mlflow_dir
+    # os.environ["HF_MLFLOW_LOG_ARTIFACTS"]="1"
+    os.environ["MLFLOW_TAGS"]='{"mlflow.note.content":' + f'"{args.expr_desc}"' + "}"
+
+
+def main(args):
+
+    ######################################### model #########################################
+    model=load_model(args.base_model_dir,gradient_checkpointing=args.gradient_checkpointing,quantization_config=None)
+    model.config.use_cache = False # use_cache is only for infernce
+
+    if model.config.max_position_embeddings<args.max_len:
+        model.config.max_position_embeddings=args.max_len
+
+    # model.config.pretraining_tp = 1 
+    
+    if not args.full_ft:
+      ## peft (lora)
+      peft_config = LoraConfig(
+              r=32,
+              lora_alpha=32,
+              lora_dropout=args.dropout_rate,
+              bias="none",
+              task_type="CAUSAL_LM",
+              target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj","down_proj","up_proj"]
+          )
+      
+      model=get_peft_model(model,peft_config)
+    #######################################################################################
+
+
+    ######################################### tokenizer & dataset #########################################
+    tokenizer=load_tokenizer(args.base_model_dir,chat_template=chat_template.chat_ml)
+
+    print(f"eos_token is : {tokenizer.eos_token}")
+    
+    train_dataset,eval_dataset=load_and_prepare_dataset(tokenizer)
+    # train_dataset=load_and_prepare_dataset(tokenizer)
+    
+    if len(tokenizer)!=int(model.config.vocab_size):
+        model.resize_token_embeddings(len(tokenizer))
+    assert len(tokenizer)==int(model.config.vocab_size) , 'vocab sizes of the tokenizer and the model should be same'
+    print(f"len tokenizer : {len(tokenizer)}")
+    print(f"model token size : {model.config.vocab_size}")
+    #######################################################################################################
+    
+    
+    ######################################## Optimizer & Scheduler #########################################
+    total_update_steps=int((len(train_dataset)*args.epoch_size)/(args.batch_size*args.gradient_accumulation_steps))
+
+    optimizer,scheduler=load_optimizer_scheduler(model=model,
+                                                learning_rate=args.learning_rate,
+                                                weight_decay=args.weight_decay,
+                                                total_update_steps=total_update_steps,
+                                                warmup_ratio=args.warmup_ratio,
+                                                quantize=True)
+    #######################################################################################################
+
+    ######################################### Trainer Setiings #########################################
+    expr_name=f"{args.expr_name}_v1"
+    output_dir= f"{args.base_dir}/trained/{expr_name}"
+
+    while os.path.exists(output_dir):
+        expr_name=expr_name.split("_v")[0]+"_v"+str(int(expr_name.split("_v")[-1])+1)
+        output_dir= f"{args.base_dir}/trained/{expr_name}"
+        
+
+    eval_steps=int(total_update_steps/args.num_save_per_epoch)
+    # eval_steps=100
+
+    training_arguments = TrainingArguments(output_dir= output_dir,
+        # fp16= True,
+        bf16= True,
+        # run_name=args.expr_desc,
+        metric_for_best_model="eval_loss",
+       # ddp_find_unused_parameters=False,
+        # torch_compile=True,
+                        )
+    training_arguments=training_arguments.set_dataloader(train_batch_size=args.batch_size,
+                                                             eval_batch_size=args.batch_size*2,
+                                                             pin_memory=True,
+                                                             num_workers=4,
+                                                             sampler_seed=args.seed)
+    training_arguments=training_arguments.set_training(
+        learning_rate= args.learning_rate,
+        batch_size=args.batch_size,
+        weight_decay=args.weight_decay,
+        num_epochs=args.epoch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        seed=args.seed,
+        )
+    training_arguments=training_arguments.set_logging(strategy="steps",steps=100,report_to=["mlflow"])
+    training_arguments=training_arguments.set_evaluate(strategy="steps", batch_size=args.batch_size*2,steps=eval_steps,delay=0)
+    training_arguments=training_arguments.set_save(strategy="steps",steps=eval_steps,total_limit=20,)
+    training_arguments.load_best_model_at_end=True
+
+    response_template_with_context = "\n### Output:\n"  # We added context here: "\n". This is enough for this tokenizer
+    response_template_ids = tokenizer.encode(response_template_with_context, add_special_tokens=False)[2:]
+    collator=DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer)
+
+    trainer = SFTTrainer(
+    args=training_arguments,
+    model=model,
+    tokenizer=tokenizer,
+    optimizers=(optimizer,scheduler),
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    # data_collator=collator,
+    peft_config=peft_config if not args.full_ft else None,
+    max_seq_length= args.max_len,
+    dataset_text_field="text",
+    # packing= True,
+    )
+    ######################################################################################################
+
+    args_table = PrettyTable(["Argument", "Value"])
+    for k,v in training_arguments.__dict__.items():
+        args_table.add_row([k,v])
+    args_table.add_row(["total_update_steps",total_update_steps])
+    print(args_table)
+
+    if args.train:
+      if args.ckpt_dir is not None:
+        trainer.train(args.ckpt_dir)
+      else:
+        trainer.train()
+        trainer.save_model(output_dir+"/full_model")    
+        
+    mlflow.end_run()
+    
+if __name__=="__main__":
+  args=parse_args()
+  seed_everything(args.seed)
+  set_environ(args)
+  main(args)
---
+++ src/utils.py
@@ -0,0 +1,119 @@
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)  # type: ignore
+    torch.backends.cudnn.deterministic = True  # type: ignore
+    torch.backends.cudnn.benchmark = True  # type: ignore
+
+def check_if_translation_orca(sample):
+
+    for d in sample:
+        if "translation" in d["value"] or "translate" in d["value"]:
+            return True
+    return False
+
+def make_translation_prompt(data,src:str=None, tgt:str=None):
+
+  lang_dict={"korean":"한국어","english":"영어","ko":"한국어","eng":"영어"}
+
+  if not src and not tgt:
+    if "src" in data.keys() and "tgt" in data.keys():
+      src=data["src"]
+      tgt=data["tgt"]
+    else:
+      raise Exception("'src'와 'tgt'가 주어지거나, data의 key로 존재해야합니다.")
+
+  if "term_dict" not in data.keys() or not len(data["term_dict"]):
+    template = f"""### Instruction:
+{lang_dict[src]}를 {lang_dict[tgt]}로 번역하시오.
+### Input:
+{data[src]}
+### Output:
+{data[tgt]}
+"""
+  else:
+    template = f"""### Instruction:
+아래의 용어사전의 참조하여, {lang_dict[src]}를 {lang_dict[tgt]}로 번역하시오.
+
+용어사전 : {data["term_dict"]}
+### Input:
+{data[src]}
+### Output:
+{data[tgt]}
+"""
+
+  return {"text":template}
+
+
+def merge_and_resort(df1,df2):
+    '''
+    df1 : origianl dataset
+    df2 : sampled dataset with term_dict
+    '''
+
+    def invert_dict(term_dict,src):
+        if term_dict is not np.nan:
+            if src=="korean":
+                return str(term_dict)
+            elif src=="english":
+                return str({v:k for k,v in term_dict.items()})
+        else:
+            return ""
+    
+    merged_df=pd.merge(df1,df2,on="id")
+    # merged_df=pd.merge(df1,df2,on="id",how="outer").drop_duplicates(subset="id")
+    merged_df=merged_df.sample(frac=1).reset_index(drop=True)
+
+    # split ko->eng and eng->ko
+    cut_off=len(merged_df)//2
+    src_tags=["korean"]*cut_off + ["english"]*(len(merged_df)-cut_off)
+    tgt_tags=["english"]*cut_off + ["korean"]*(len(merged_df)-cut_off)
+    merged_df["src"]=src_tags
+    merged_df["tgt"]=tgt_tags
+
+    term_dict=[invert_dict(t_d,s) for t_d,s in zip(merged_df["term_dict"].values,merged_df["src"].values)]
+    merged_df["term_dict"]=term_dict
+    merged_df=merged_df.sample(frac=1).reset_index(drop=True)
+
+    return merged_df
+
+
+def filter_valid_data(data):
+
+    errors=[]
+    valid_list=[]
+    
+    for idx,d in enumerate(data):
+        try:
+            word_dict=json.loads(d["maps"])
+            valid_list.append({"id":d["id"],
+                            "term_dict":word_dict})
+        except:
+            errors.append(idx)
+
+
+    return valid_list,errors
+
+
+def prepare_translation_dataset(raw_dataset_path,term_dict_path):
+    dataset=Dataset.load_from_disk(raw_dataset_path)
+    df1=pd.DataFrame(dataset)
+
+    term_dict_data=[]
+    with jsonlines.open(term_dict_path) as f:
+        for line in f:
+            term_dict_data.append(line)
+    
+    valid_data=filter_valid_data(data)[0]
+    df2=pd.DataFrame(valid_data)
+    merged_df=merge_and_resort(df1,df2)
+
+    return Dataset.from_pandas(merged_df)
+    
+    
+    
+    
+
+
