6efdb44f71dc450cc662dc42fa70be3c9c038472
jh-lee95 <skdltmwkd12@gmail.com>
2024-04-23 17:20:36 +0900

    prompt minor change

--- src/prompt_template.py
+++ src/prompt_template.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class TranslationTemplate:
-    translation_template='''You are a professional translator. Translate the following {} source text into {}. Refer to the word pairs in the glossary if the glossary exists when you translate. Do not translate the glossary it self.
+    translation_template='''You are a professional translator. Translate the following {} source text into {}. Refer to the word pairs in the glossary if the glossary exists when you translate. Do not translate the glossary itself.
 {}
 Translation:
 '''
